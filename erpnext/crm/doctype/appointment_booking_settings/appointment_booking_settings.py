300d476c392d97f58f965e674e67e469a9876115
Daizy Modi <modidaizy5217@gmail.com>
2023-01-09 12:21:27 +0530

    refactor: appointment booking settings

--- erpnext/crm/doctype/appointment_booking_settings/appointment_booking_settings.py
+++ erpnext/crm/doctype/appointment_booking_settings/appointment_booking_settings.py
@@ -2,41 +2,35 @@
 # For license information, please see license.txt
 
 
-import datetime
-
 import frappe
 from frappe import _
 from frappe.model.document import Document
+from frappe.utils.data import get_datetime
 
 
 class AppointmentBookingSettings(Document):
-	agent_list = []  # Hack
-	min_date = "01/01/1970 "
-	format_string = "%d/%m/%Y %H:%M:%S"
-
 	def validate(self):
 		self.validate_availability_of_slots()
+		self.set_total_agents()
 
-	def save(self):
+	def set_total_agents(self):
 		self.number_of_agents = len(self.agent_list)
-		super(AppointmentBookingSettings, self).save()
 
 	def validate_availability_of_slots(self):
 		for record in self.availability_of_slots:
-			from_time = datetime.datetime.strptime(self.min_date + record.from_time, self.format_string)
-			to_time = datetime.datetime.strptime(self.min_date + record.to_time, self.format_string)
-			timedelta = to_time - from_time
-			self.validate_from_and_to_time(from_time, to_time, record)
-			self.duration_is_divisible(from_time, to_time)
+			from_time = get_datetime(record.from_time)
+			to_time = get_datetime(record.to_time)
 
-	def validate_from_and_to_time(self, from_time, to_time, record):
-		if from_time > to_time:
-			err_msg = _("<b>From Time</b> cannot be later than <b>To Time</b> for {0}").format(
-				record.day_of_week
-			)
-			frappe.throw(_(err_msg))
+			if from_time > to_time:
+				frappe.throw(
+					_("Row #{0}: <b>To Time</b> must be greater than <b>From Time</b> for {1}").format(
+						record.idx, record.day_of_week
+					)
+				)
 
-	def duration_is_divisible(self, from_time, to_time):
+			self.is_duration_divisible(from_time, to_time)
+
+	def is_duration_divisible(self, from_time, to_time):
 		timedelta = to_time - from_time
 		if timedelta.total_seconds() % (self.appointment_duration * 60):
 			frappe.throw(
